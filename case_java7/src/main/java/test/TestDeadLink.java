1d745f0d43ac0bb8a2dc51116e8dd73818c273d9
zengrui <772701414@qq.com>
2021-04-30 17:04:09 +0800

    测试死锁

--- case_java7/src/main/java/test/TestDeadLink.java
+++ case_java7/src/main/java/test/TestDeadLink.java
@@ -17,28 +17,37 @@ public class TestDeadLink {
                 System.out.println(i);
             }
         }
+
         // 1, 35, 16, 50 当大小为16时，它们在一个桶内
-        final HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        final HashMap<Integer, String> map = new HashMap<>();
         // 放 12 个元素
-        map.put(2, null);
-        map.put(3, null);
-        map.put(4, null);
-        map.put(5, null);
-        map.put(6, null);
-        map.put(7, null);
-        map.put(8, null);
-        map.put(9, null);
-        map.put(10, null);
-        map.put(16, null);
-        map.put(35, null);
-        map.put(1, null);
+        map.put(2, "s2");
+        map.put(3, "s3");
+        map.put(4, "s4");
+        map.put(5, "s5");
+        map.put(6, "s6");
+        map.put(7, "s7");
+        map.put(8, "s8");
+        map.put(9, "s9");
+        map.put(10, "s10");
+        // 扩容钱1,35,16在一条链
+        // 扩容后 1,35 还在一条链
+        map.put(16, "s16");
+        map.put(35, "s35");
+        map.put(1, "s1");
 
         System.out.println("扩容前大小[main]:"+map.size());
+
+        /*
+         * 发生扩容：元素个数大于3/4个数的时候开始扩容，原来16长度的，最多存放12个元素，增加到13开始扩容
+         * 多存放一个，放13的元素开始扩容
+         * 使用两个线程增加第13个元素
+         */
         new Thread() {
             @Override
             public void run() {
                 // 放第 13 个元素, 发生扩容
-                map.put(50, null);
+                map.put(50, "s50t0");
                 System.out.println("扩容后大小[Thread-0]:"+map.size());
             }
         }.start();
@@ -46,7 +55,7 @@ public class TestDeadLink {
             @Override
             public void run() {
                 // 放第 13 个元素, 发生扩容
-                map.put(50, null);
+                map.put(50, "s50t1");
                 System.out.println("扩容后大小[Thread-1]:"+map.size());
             }
         }.start();
